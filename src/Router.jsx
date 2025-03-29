bcf1c6f7afcf6ae438b96cffb734c3907043970a
facundo brunetti <facundobrunetti99@gmail.com>
2025-03-29 09:16:57 -0300

    installation dependencies

---
+++ src/Router.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Routes, Route, Navigate} from 'react-router-dom'
+const Router = () => {
+  return (
+    <Routes>
+    
+    
+    
+    
+    </Routes>
+  )
+}
+
+export default Router
