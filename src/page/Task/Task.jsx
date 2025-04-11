6f7e03eb3340a834fdefaf8317ce8e46a76cef59
Facundo Brunetti <facundobrunetti99@gmail.com>
2025-04-11 20:39:39 -0300

    added task

--- src/page/Task/Task.jsx
+++ src/page/Task/Task.jsx
@@ -3,6 +3,67 @@ import Title from '../../component/Tittle/Title'
 import { useParams } from 'react-router-dom'
 import "../Task/Task.css"
 const Task = () => {
+  const task = {
+    "1.1.1": [{
+        id:"1.1.1.1",
+        name:"Stories 1.1",
+        cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+        
+     },
+     {
+      id:"1.1.1.2",
+      name:"Stories 1.2",
+      cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+      
+      },
+    {
+      id:"1.1.1.3",
+      name:"Stories 1.3",
+      cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+      
+    }],
+    "1.1.2": [
+      {
+        id:"1.1.2.1",
+        name:"Stories 2.1",
+        cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+        
+     },
+     {
+      id:"1.1.2.2",
+      name:"Stories 1.2.2",
+      cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+      
+      },
+    {
+      id:"1.1.2.3",
+      name:"Stories 1.2.3",
+      cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+      
+    }
+    ],
+    "1.1.1.3": [{
+      id:"1.3.1",
+      name:"Stories 1.3.1",
+      cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+      
+   },
+   {
+    id:"1.3.2",
+    name:"Stories 1.3.2",
+    cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+    
+    },
+  {
+    id:"1.3.3",
+    name:"Stories 1.3.3",
+    cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+    
+  }],
+    
+  };
+
+
 
   const{storieId} = useParams();
   return (
