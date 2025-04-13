a878d30bffff59539bf344b1bb6398e161d2efb7
Facundo Brunetti <facundobrunetti99@gmail.com>
2025-04-13 19:21:22 -0300

    add

--- src/page/Stories/Stories.jsx
+++ src/page/Stories/Stories.jsx
@@ -1,91 +1,87 @@
-import React from 'react'
-import '../Stories/Stories.css'
-import { Link, useParams } from 'react-router-dom';
-
+import React from "react";
+import "../Stories/Stories.css";
+import { Link, useParams } from "react-router-dom";
 
 const stories = {
-    1.1: [{
-        id:"1.1.1",
-        name:"Stories 1.1",
-        cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
-        
-     },
-     {
-      id:"1.1.2",
-      name:"Stories 1.2",
-      cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
-      
-      },
+  1.1: [
     {
-      id:"1.1.3",
-      name:"Stories 1.3",
-      cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
-      
-    }],
-    1.2: [
-      {
-        id:"1.2.1",
-        name:"Stories 2.1",
-        cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
-        
-     },
-     {
-      id:"1.2.2",
-      name:"Stories 1.2.2",
-      cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
-      
-      },
-    {
-      id:"1.2.3",
-      name:"Stories 1.2.3",
-      cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
-      
-    }
-    ],
-    1.3: [{
-      id:"1.3.1",
-      name:"Stories 1.3.1",
-      cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
-      
-   },
-   {
-    id:"1.3.2",
-    name:"Stories 1.3.2",
-    cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
-    
+      id: "1.1.1",
+      name: "Stories 1.1",
+      cont: "Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
     },
-  {
-    id:"1.3.3",
-    name:"Stories 1.3.3",
-    cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
-    
-  }],
-    
-  };
-  
-const Stories = () => {
-   const {epicId}=useParams();
-   const st= stories[epicId] || [];
-   let m=stories[epicId].length;
-  return (
-<div className='st-main'>
-<p className='st-main-title'>Stories del Proyecto {epicId}</p>
-<div className="content-date-stories">
-<p className='title-st-item'>Stories del usuario: {"Facundo"}</p>
-<p className="tittle-st-item">Cantidad de stories: {m}</p>
-<p className="tittle-st-item">Fecha de inicio: 20/03/2025</p>
-</div>
-<div className='div-st-item'>
-  {st.map((sto, index) => (
-    <Link className='link-st-content' to={`/project/epics/stories/task/${sto.id}`}  key={index}>
-    <p className='title-st-item'>{sto.name}</p>
-    <p className='contenet-st-item'>{sto.cont}</p>
- 
-  </Link>
-  ))}
-</div>
-</div>
-  )
-}
+    {
+      id: "1.1.2",
+      name: "Stories 1.2",
+      cont: "Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+    },
+    {
+      id: "1.1.3",
+      name: "Stories 1.3",
+      cont: "Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+    },
+  ],
+  1.2: [
+    {
+      id: "1.2.1",
+      name: "Stories 2.1",
+      cont: "Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+    },
+    {
+      id: "1.2.2",
+      name: "Stories 1.2.2",
+      cont: "Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+    },
+    {
+      id: "1.2.3",
+      name: "Stories 1.2.3",
+      cont: "Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+    },
+  ],
+  1.3: [
+    {
+      id: "1.3.1",
+      name: "Stories 1.3.1",
+      cont: "Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+    },
+    {
+      id: "1.3.2",
+      name: "Stories 1.3.2",
+      cont: "Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+    },
+    {
+      id: "1.3.3",
+      name: "Stories 1.3.3",
+      cont: "Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+    },
+  ],
+};
 
-export default Stories
+const Stories = () => {
+  const { epicId } = useParams();
+  const st = stories[epicId] || [];
+  let m = stories[epicId].length;
+  return (
+    <div className="st-main">
+      <p className="st-main-title">Stories del Proyecto {epicId}</p>
+      <div className="content-date-stories">
+        <p className="title-st-item">Stories del usuario: {"Facundo"}</p>
+        <p className="tittle-st-item">Cantidad de stories: {m}</p>
+        <p className="tittle-st-item">Fecha de inicio: 20/03/2025</p>
+      </div>
+      <div className="div-st-item">
+        {st.map((sto, index) => (
+          <Link
+            className="link-st-content"
+            to={`/project/epics/stories/task/${sto.id}`}
+            key={index}
+          >
+            <p className="title-st-item">{sto.name}</p>
+            <p className="contenet-st-item">{sto.cont}</p>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Stories;
--- src/page/Task/Task.jsx
+++ src/page/Task/Task.jsx
@@ -1,84 +1,74 @@
-import React from 'react'
-import Title from '../../component/Tittle/Title'
-import { useParams } from 'react-router-dom'
-import "../Task/Task.css"
+import React from "react";
+import Title from "../../component/Tittle/Title";
+import { useParams } from "react-router-dom";
+import "../Task/Task.css";
 const Task = () => {
   const task = {
-    "1.1.1": [{
-        id:"1.1.1.1",
-        name:"Stories 1.1",
-        cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
-        
-     },
-     {
-      id:"1.1.1.2",
-      name:"Stories 1.2",
-      cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
-      
+    "1.1.1": [
+      {
+        id: "1.1.1.1",
+        name: "Stories 1.1",
+        cont: "Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
       },
-    {
-      id:"1.1.1.3",
-      name:"Stories 1.3",
-      cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
-      
-    }],
+      {
+        id: "1.1.1.2",
+        name: "Stories 1.2",
+        cont: "Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+      },
+      {
+        id: "1.1.1.3",
+        name: "Stories 1.3",
+        cont: "Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+      },
+    ],
     "1.1.2": [
       {
-        id:"1.1.2.1",
-        name:"Stories 2.1",
-        cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
-        
-     },
-     {
-      id:"1.1.2.2",
-      name:"Stories 1.2.2",
-      cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
-      
+        id: "1.1.2.1",
+        name: "Stories 2.1",
+        cont: "Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
       },
-    {
-      id:"1.1.2.3",
-      name:"Stories 1.2.3",
-      cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
-      
-    }
+      {
+        id: "1.1.2.2",
+        name: "Stories 1.2.2",
+        cont: "Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+      },
+      {
+        id: "1.1.2.3",
+        name: "Stories 1.2.3",
+        cont: "Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+      },
     ],
-    "1.1.1.3": [{
-      id:"1.3.1",
-      name:"Stories 1.3.1",
-      cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
-      
-   },
-   {
-    id:"1.3.2",
-    name:"Stories 1.3.2",
-    cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
-    
-    },
-  {
-    id:"1.3.3",
-    name:"Stories 1.3.3",
-    cont:"Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
-    
-  }],
-    
+    "1.1.1.3": [
+      {
+        id: "1.3.1",
+        name: "Stories 1.3.1",
+        cont: "Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+      },
+      {
+        id: "1.3.2",
+        name: "Stories 1.3.2",
+        cont: "Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+      },
+      {
+        id: "1.3.3",
+        name: "Stories 1.3.3",
+        cont: "Desarrollar un panel de administración intuitivo para gestionar usuarios, roles y permisos, mejorando la seguridad y eficiencia del sistema.",
+      },
+    ],
   };
 
-
-
-  const{storieId} = useParams();
+  const { storieId } = useParams();
   return (
     <div>
       <Title clas={"title-public-home"} title={"Tareas"}></Title>
       <div className="content-task">
-      <p>Tareas a realizar para la storie {storieId}</p> 
-       <p>Nombre del usuario: Facundo</p>
-       <p>Cantidad de tareas:</p>
-       <p>Tareas realizadas: </p>
+        <p>Tareas a realizar para la storie {storieId}</p>
+        <p>Nombre del usuario: Facundo</p>
+        <p>Cantidad de tareas:</p>
+        <p>Tareas realizadas: </p>
       </div>
-     
-
     </div>
-  )
-}
+  );
+};
 
-export default Task
+export default Task;
